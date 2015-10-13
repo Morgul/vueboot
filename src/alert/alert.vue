9fe39cea411041a1936e4fe0b03749603348455a
Christopher S. Case <chris.case@g33xnexus.com>
2015-10-13 12:21:27 -0500

    Added alert component.

---
+++ src/alert/alert.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="alert alert-{{ type }}" :class="{ 'alert-dismissible': dismissible, fade: animation, in: animation }" role="alert">
+        <button v-if="dismissible" type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            <span class="sr-only">Close</span>
+        </button>
+        <slot></slot>
+    </div>
+</template>
+
+<script type="text/babel" lang="es6">
+    export default {
+        props: {
+            type: {
+                type: String,
+                required: true
+            },
+            dismissible: {
+                type: Boolean,
+                default: true
+            },
+            animation: {
+                type: Boolean,
+                default: true
+            },
+            timeout: {
+                type: Number,
+                default: -1
+            },
+            onClosed: {
+                type: Function,
+                default: () => {}
+            }
+        },
+        methods: {
+            close: function()
+            {
+                $(this.$el).alert('close');
+            }
+        },
+        ready: function()
+        {
+            if(this.timeout > -1)
+            {
+                setTimeout(() => { this.close() }, this.timeout)
+            } // end if
+
+            $(this.$el).on('closed.bs.alert', () =>
+            {
+                this.onClosed();
+            });
+        }
+    }
+</script>
