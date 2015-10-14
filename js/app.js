7cb0f1e19d2de3b0e67f1149af536dc190ed6d5a
Christopher S. Case <chris.case@g33xnexus.com>
2015-10-13 19:22:55 -0500

    added alert.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@
 var app = new Vue({
     debug: true,
     el: '#example-app',
+    data: {
+        showTimedAlert: false
+    },
     methods: {
         showModal: function(name)
         {
@@ -13,9 +16,16 @@ var app = new Vue({
         hideModal: function(name)
         {
             this.$refs[name].hideModal();
+        },
+        showAlert: function(){ this.showTimedAlert = true; },
+        onClosed: function()
+        {
+            console.log('onClosed!');
+            this.showTimedAlert = false;
         }
     },
     components: {
+        alert: vueboot.alert,
         modal: vueboot.modal
     }
 });
