3a8195ef7137db4a4e552c763852c19ea9b43450
Christopher S. Case <chris.case@g33xnexus.com>
2015-10-13 19:44:10 -0500

    0.1.0

--- dist/vueboot.js
+++ dist/vueboot.js
@@ -1,4 +1,4 @@
-/* Vuestrap v0.1.0 */
+/* VueBoot v0.1.0 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.vueboot = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var __vue_template__ = "<div class=\"alert alert-{{ type }}\" :class=\"{ 'alert-dismissible': dismissible, fade: animation, in: animation }\" role=\"alert\">\n        <button v-if=\"dismissible\" type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">\n            <span aria-hidden=\"true\">×</span>\n            <span class=\"sr-only\">Close</span>\n        </button>\n        <slot></slot>\n    </div>";
 'use strict';
@@ -37,7 +37,7 @@ exports['default'] = {
     ready: function ready() {
         var _this = this;
 
-        if (this.timeout > -1) {
+        if (this.timeout >= 0) {
             setTimeout(function () {
                 _this.dismiss();
             }, this.timeout);
