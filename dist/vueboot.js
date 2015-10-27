ce9720d71ea84b7e874d5dff2413f9b3e1d7ac4c
Christopher S. Case <chris.case@g33xnexus.com>
2015-10-27 12:12:41 -0500

    updated to latest VueBoot.

--- dist/vueboot.js
+++ dist/vueboot.js
@@ -1,5 +1,5 @@
-/* Vuestrap v0.1.0 */
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.vueboot = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/* VueBoot v0.1.2 */
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.vueboot = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 var __vue_template__ = "<div class=\"alert alert-{{ type }}\" :class=\"{ 'alert-dismissible': dismissible, fade: animation, in: animation }\" role=\"alert\">\n        <button v-if=\"dismissible\" type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">\n            <span aria-hidden=\"true\">×</span>\n            <span class=\"sr-only\">Close</span>\n        </button>\n        <slot></slot>\n    </div>";
 'use strict';
 
@@ -51,7 +51,7 @@ exports['default'] = {
 module.exports = exports['default'];
 ;(typeof module.exports === "function"? module.exports.options: module.exports).template = __vue_template__;
 
-},{}],2:[function(require,module,exports){
+},{}],2:[function(_dereq_,module,exports){
 var __vue_template__ = "<div class=\"modal\" :class=\"{ fade: animation }\" tabindex=\"-1\" role=\"dialog\" aria-hidden=\"true\" data-backdrop=\"{{ backdrop }}\" data-keyboard=\"{{ keyboard }}\">\n        <div class=\"modal-dialog\" role=\"document\" :style=\"{ width: width }\">\n            <div class=\"modal-content\">\n                <slot name=\"header\"></slot>\n                <slot name=\"body\"></slot>\n                <slot name=\"footer\"></slot>\n            </div>\n        </div>\n    </div>";
 'use strict';
 
@@ -114,11 +114,13 @@ exports['default'] = {
         var _this = this;
 
         $(this.$el).on('shown.bs.modal', function () {
-            var autoElem = $(_this.autoFocus);
-            if (autoElem[0]) {
-                autoElem.focus();
-            } else {
-                console.warn('[VueBoot] Autofocus selector \'' + _this.autoFocus + '\' did not select an element.');
+            if (_this.autoFocus) {
+                var autoElem = $(_this.autoFocus);
+                if (autoElem[0]) {
+                    autoElem.focus();
+                } else {
+                    console.warn('[VueBoot] Autofocus selector \'' + _this.autoFocus + '\' did not select an element.');
+                } // end if
             } // end if
         });
 
@@ -131,7 +133,7 @@ exports['default'] = {
 module.exports = exports['default'];
 ;(typeof module.exports === "function"? module.exports.options: module.exports).template = __vue_template__;
 
-},{}],3:[function(require,module,exports){
+},{}],3:[function(_dereq_,module,exports){
 //----------------------------------------------------------------------------------------------------------------------
 /// Main VueBoot module
 ///
@@ -146,11 +148,11 @@ Object.defineProperty(exports, "__esModule", {
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var _alertAlertVue = require("./alert/alert.vue");
+var _alertAlertVue = _dereq_("./alert/alert.vue");
 
 var _alertAlertVue2 = _interopRequireDefault(_alertAlertVue);
 
-var _modalModalVue = require("./modal/modal.vue");
+var _modalModalVue = _dereq_("./modal/modal.vue");
 
 var _modalModalVue2 = _interopRequireDefault(_modalModalVue);
 
